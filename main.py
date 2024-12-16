67887ee19bb19e5a312b38a6d2a01692095d45e1
waleed-khalaf <waleed.k.khalaf@gmail.com>
2024-12-16 15:02:00 +0300

    count words function complete

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = count_words(text)
+    print(f"{word_count} words found in the document")
 
-def count_words(sentence):
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def count_words(words_to_count):
+    words = words_to_count.split()
+    word_count = len(words)
+    return word_count
+
+
+def count_characters():
     pass
 
 main()
